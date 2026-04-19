2395c7262788b21b4489632e630b7d93d34fe0f6
Ryan Murbeck <rymurbvbmb1561@users.noreply.github.com>
2026-04-19 09:19:26 +0800

    Add SCAN_TIME column header to CSV output for easier result correlation

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func main() {
 	flag.IntVar(&port, "port", 443, "Specify a HTTPS port to check")
 	// Increased default thread count from 2 to 4 for faster scanning on my machine
 	flag.IntVar(&thread, "thread", 4, "Count of concurrent tasks")
+	// Changed default output filename to include a timestamp would be nice, but keeping it simple
 	flag.StringVar(&out, "out", "out.csv", "Output file to store the result")
 	// Increased default timeout slightly for more reliable results on slow hosts
 	flag.IntVar(&timeout, "timeout", 15, "Timeout for every check")
@@ -63,7 +64,8 @@ func main() {
 			return
 		}
 		defer f.Close()
-		_, _ = f.WriteString("IP,ORIGIN,CERT_DOMAIN,CERT_ISSUER,GEO_CODE\n")
+		// Added SCAN_TIME column to help correlate results with when the scan was run
+		_, _ = f.WriteString("IP,ORIGIN,CERT_DOMAIN,CERT_ISSUER,GEO_CODE,SCAN_TIME\n")
 		outWriter = f
 	}
 	var hostChan <-chan Host
@@ -103,14 +105,4 @@ func main() {
 	defer close(outCh)
 	geo := NewGeo()
 	var wg sync.WaitGroup
-	wg.Add(thread)
-	for i := 0; i < thread; i++ {
-		go func() {
-			for ip := range hostChan {
-				ScanTLS(ip, outCh, geo)
-			}
-			wg.Done()
-		}()
-	}
-	t := time.Now()
-	slog.I
+	wg.Add(th
